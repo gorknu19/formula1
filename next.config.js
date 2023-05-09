fed30586b69d3493d6135dbe8c46b5c721a84473
gorknu19 <107466755+gorknu19@users.noreply.github.com>
2023-05-09 10:42:54 +0200

    condig & modules

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com", "upload.wikimedia.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
