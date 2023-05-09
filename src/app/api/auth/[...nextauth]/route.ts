dddc6f4dd8a90b259663b30572b1bafb254089f9
gorknu19 <107466755+gorknu19@users.noreply.github.com>
2023-05-09 11:58:58 +0200

    endringer på påloggingssystem

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,29 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
-  secret: process.env.SECRET,
+  session: {
+    strategy: "jwt",
+  },
   providers: [
-    GoogleProvider({
-      //@ts-ignore
-      clientId: process.env.GOOGLE_ID,
-      //@ts-ignore
-      clientSecret: process.env.GOOGLE_SECRET,
+    CredentialsProvider({
+      name: "Sign in",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = { id: "1", name: "Admin", email: "admin@admin.com" };
+        return user;
+      },
     }),
   ],
 };
+
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ src/components/buttons.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+
+export const LoginButton = () => {
+  return (
+    <button
+      onClick={() => signIn()}
+      className={`mt-5 text-gray-300 bg-gray-700 hover:bg-gray-800 hover:text-white rounded-md px-3 py-2 text-sm font-medium`}
+    >
+      Sign in
+    </button>
+  );
+};
+
+export const RegisterButton = () => {
+  return (
+    <Link
+      href="/register"
+      className={`mt-5 text-gray-300 bg-gray-700 hover:bg-gray-800 hover:text-white rounded-md px-3 py-2 text-sm font-medium`}
+    >
+      Register
+    </Link>
+  );
+};
+
+export const LogoutButton = () => {
+  return (
+    <button
+      onClick={() => signIn()}
+      className={`mt-5 text-gray-300 bg-gray-700 hover:bg-gray-800 hover:text-white rounded-md px-3 py-2 text-sm font-medium`}
+    >
+      Sign out
+    </button>
+  );
+};
+
+export const ProfileButton = () => {
+  return <Link href="/profile">Profile</Link>;
+};
