835dcde0aefb95ad9e307ddda1d2ee7b211f3262
gorknu19 <107466755+gorknu19@users.noreply.github.com>
2023-05-10 10:10:06 +0200

    fjernet fil, lagd secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,6 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.SECRET,
   providers: [
     CredentialsProvider({
       name: "Sign in",
@@ -39,11 +38,35 @@ export const authOptions: NextAuthOptions = {
           id: user.id,
           email: user.email,
           name: user.name,
-          randomKey: "Hey cool",
         };
       },
     }),
   ],
+  callbacks: {
+    session: ({ session, token }) => {
+      console.log("Session Callback", { session, token });
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          randomKey: token.randomKey,
+        },
+      };
+    },
+    jwt: ({ token, user }) => {
+      console.log("JWT Callback", { token, user });
+      if (user) {
+        const u = user as unknown as any;
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey,
+        };
+      }
+      return token;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+"use client";
 import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
   return <main className={`flex min-h-screen bg-slate-600`}></main>;
 }
